97c4ef760dfb7eebd0dce120b6cbf0015c6b56e9
iulianiosubcode11 <iulian.iosub@code11.com>
2024-10-14 18:29:26 +0300

    s

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,29 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Flex, Box } from "@chakra-ui/react";
+import { Flex, Box, Stack, HStack, VStack } from "@chakra-ui/react";
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<>
-			<Flex justify="space-between" align='center' w='600px' bg='grey'>
+			<HStack w='600px' bg='grey'>
+				<Flex bg="green" w="120px" h="200px">
+					This is a Box
+				</Flex>
+				<Flex bg="blue" w="120px" h="200px">
+					This is a Box
+				</Flex>
+				<Flex bg="yellow" w="100px" h="250px">
+					This is a Box
+				</Flex>
+				<Flex bg="brown" w="120px" h="200px">
+					This is a Box
+				</Flex>
+			</HStack>
+
+      <Flex justify="space-between" align='center' w='600px' bg='grey'>
 				<Flex bg="green" w="120px" h="200px">
 					This is a Box
 				</Flex>
