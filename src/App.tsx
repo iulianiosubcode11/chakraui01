4bba5899383a78e006011418d51cf87e37eab996
iulianiosubcode11 <iulian.iosub@code11.com>
2024-10-14 23:54:12 +0300

    final prima parte

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Flex, Box, Stack, HStack, VStack } from "@chakra-ui/react";
+import { Flex, Box, Stack, HStack, VStack, Heading, Button } from "@chakra-ui/react";
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<>
-			<HStack w='600px' bg='grey'>
+			<Heading as="H1" fontSize="54" pb="32px">
+				Lorem ipsum dolor sit amet
+			</Heading>
+      <Button mb='48px' color="#376b90" bgColor='#945577' _hover={{ background: '#fff000', color: '#000' }}>Push me</Button>
+			<Stack justify="space-between" w="600px" bg="grey" margin='auto' mb='30px' direction="row-reverse" align='center'>
 				<Flex bg="green" w="120px" h="200px">
 					This is a Box
 				</Flex>
@@ -22,9 +26,9 @@ function App() {
 				<Flex bg="brown" w="120px" h="200px">
 					This is a Box
 				</Flex>
-			</HStack>
+			</Stack>
 
-      <Flex justify="space-between" align='center' w='600px' bg='grey'>
+			<Flex justify="space-between" align="center" w="600px" bg="grey" margin='auto'>
 				<Flex bg="green" w="120px" h="200px">
 					This is a Box
 				</Flex>
